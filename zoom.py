422ca978121f10f1ea535d0bb2d378732b0ee8b3
TowyZ <82327398+TowyZheng@users.noreply.github.com>
2026-06-22 17:04:51 -0700

    Update zoom.py
    Updated setup() and __init__()

--- zoom.py
+++ zoom.py
@@ -1,19 +1,22 @@
-from astropy.io import fits
+from astropy.io import fits  # importing i/o subpackage for reading FITS files
+
+import astropy.units as u # astropy units
 
 class FITSZoom(object):
 
     def __init__(self, filename):
         
-        self.filename = filename
-        self.fits = fits.open(filename)
-
+        self.filename = filename             
+        self.fits = fits.open(filename) # open the fits file --> this returns as a list of Header and Data units
+        self.setup() # pull out the data we need so that they are ready to use right after construction
+    
     def setup(self):
         """
         Extract the necessary attributes from the fits header
         """
-
+        self.header = self.fits[0].header
+        self.data = self.fits[0].data
         ### TO DO ###
-
         return 
     
     def zoom(self, coordinates, cutout_dimensions):
