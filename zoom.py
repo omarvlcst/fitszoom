e2ec4d6d0201c1b6c4ab31c541c343f4f16711ab
Haille <hmlperkins@hotmail.com>
2026-06-23 13:54:42 -0700

    zoom with astropy

--- zoom.py
+++ zoom.py
@@ -1,6 +1,7 @@
 from astropy.io import fits  # importing i/o subpackage for reading FITS files
-
+from astropy.nddata import Cutout2D
 import astropy.units as u # astropy units
+from astropy.wcs import WCS
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -19,10 +20,13 @@ class FITSZoom(object):
 
         Args:
             extension: the index where the desired data is within the fits
+            alternative_names: dictionary mapping the names of appropriate values
         """
         self.header = self.fits[extension].header
         self.data = self.fits[extension].data
-        ### TO DO ###
+        self.wcs = WCS(self.header)
+        ### TO DO ##
+
         return 
     
     def zoom(self, coordinates, cutout_dimensions):
@@ -34,16 +38,23 @@ class FITSZoom(object):
             cutout_dimensions (tuple): dimensions 
         """
 
-        ### TO DO ###
-        return 
+        if cutout_dimensions[0].unit != u.arcsec or cutout_dimensions[1].unit != u.arcsec:
+            raise ValueError("please use arcseconds for the dimensions")
+
+        cutout = Cutout2D(self.data, coordinates, cutout_dimensions, wcs=self.wcs)
+        
+        ax = self.plot(data=cutout.data)
+        return ax
     
-    def plot(self, **kwargs):
+    def plot(self, data=None, **kwargs):
         """
         Use matplotlib imshow to display the cutout
 
         Returns:
             axes: the plt axes with the image
         """
+        if data is None:
+            data = self.data
 
         # set some defaults
         if not kwargs.get('vmin', False):
@@ -53,6 +64,6 @@ class FITSZoom(object):
             kwargs['vmax'] = np.percentile(self.data, 99)
     
 
-        plt.imshow(self.data, **kwargs)
+        plt.imshow(data, **kwargs)
 
         return plt.gca()
