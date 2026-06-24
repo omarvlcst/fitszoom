c01032dea1fa4af10f491c1435c8555d29411074
Omar <omarvlcst@gmail.com>
2026-06-24 11:39:48 -0600

    string for images dir corrected

--- fitszoom/__init__.py
+++ fitszoom/__init__.py
@@ -4,4 +4,4 @@ __version__ = "0.0.1"
 
 #set directories where input images are located and outputs are sent to
 fitszoom_dir = os.path.dirname(__file__)
-DATADIR = os.path.join(fitszoom_dir, "../images")
+DATADIR = os.path.join(fitszoom_dir, "images/")
