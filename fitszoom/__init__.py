4218fc0564447d8cd7865065042fdbacd46389e1
Omar <omarvlcst@gmail.com>
2026-06-24 11:35:57 -0600

    first edit to init.py

---
+++ fitszoom/__init__.py
@@ -0,0 +1,7 @@
+import os
+
+__version__ = "0.0.1"
+
+#set directories where input images are located and outputs are sent to
+fitszoom_dir = os.path.dirname(__file__)
+DATADIR = os.path.join(fitszoom_dir, "../images")
