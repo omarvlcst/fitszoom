2e2a9322e3db88835f3378def2517df7330737c0
Christa DeCoursey <cndecoursey@arizona.edu>
2026-06-25 13:36:50 -0700

    added extension parameter to init function

--- fitszoom/zoom.py
+++ fitszoom/zoom.py
@@ -11,11 +11,15 @@ class FITSZoom(object):
     Zoom into the given coordinates and create a cutout with the given dimensions
     """
 
-    def __init__(self, filename):
-        
+    def __init__(self, filename, extension=0):
+        """
+        Enter your file directory and your file name should be in .fits format in the DATADIR folder defined in __init__.py.
+        Args:
+            filename (string): your file name -> "{DATADIR}/<<your image>>.fits"
+        """
         self.filename = filename             
         self.fits = fits.open(filename) # open the fits file --> this returns as a list of Header and Data units
-        self.setup() # pull out the data we need so that they are ready to use right after construction
+        self.setup(extension=extension) # pull out the data we need so that they are ready to use right after construction
     
     def setup(self, extension=0):
         """
@@ -33,6 +37,7 @@ class FITSZoom(object):
         sky1 = self.wcs.pixel_to_world(0,0)
         sky2 = self.wcs.pixel_to_world(-1,-1)
         print("---------- Recommendations ------------")
+        print("---------- Use the ra and dec within the following range ------------")
         print(f"ra between {sky1.ra.value:.6f} and {sky2.ra.value:.6f}.")
         print(f"dec between {sky1.dec.value:.6f} and {sky2.dec.value:.6f}.")
         return 
@@ -42,22 +47,30 @@ class FITSZoom(object):
         Zoom into the given coordinates and create a cutout with the given dimensions
 
         Args:
-            coordinates (tuple): ra, dec
+            coordinates (tuple): ra, dec in decimal degrees
             cutout_dimensions (tuple): dimensions 
+            
+        Return:
+            axes: plt.plot of the cutout image
+            cutout: 
         """
 
         if cutout_dimensions[0].unit != u.arcsec or cutout_dimensions[1].unit != u.arcsec:
             raise ValueError("please use arcseconds for the dimensions")
-
+        
         cutout = Cutout2D(self.data, coordinates, cutout_dimensions, wcs=self.wcs)
         
-        ax = self.plot(data=cutout.data, save=save)
+        ax = self.plot(data=cutout.data,wcs=cutout.wcs, save=save)
 
         return ax, cutout
+    
     
     def plot(self, data=None, save=False, **kwargs):
         """
         Use matplotlib imshow to display the cutout
+        
+        Args:
+            data : data from FITS image
 
         Returns:
             axes: the plt axes with the image
@@ -77,10 +90,8 @@ class FITSZoom(object):
         ax.imshow(data, **kwargs, origin='lower')
         ax.grid(color='white', ls='solid')
 
-        if save == True:
-            fig.savefig("zoomed.png")
-
         return plt.gca()
+    
 
     def save(self, cutout, output_filename):
         """
